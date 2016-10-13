10f41fa01d1d81143cfa782b8d3c8cbfe6447f8b
damsSanch <damian2gom@gmail.com>
2016-10-13 18:51:25 -0500

    correcion de clase animateds

--- views/site/index.php
+++ views/site/index.php
@@ -18,13 +18,13 @@ $this->title = 'Registrarse';
 	<div class="aqua-body">
                            <?php $form = ActiveForm::begin(['id' => 'registro-form', 'options' => ['class' => 'form-login']]); ?>
 	
-				<?= $form->field($usuario, 'txt_nombre',['options'=>['class'=>'animated']])->textInput(['autofocus' => true]) -> label(false) -> textInput(["placeholder" => "Nombre", "class"=>"animateds"])?>
+				<?= $form->field($usuario, 'txt_nombre',['options'=>['class'=>'animated']])->textInput(['autofocus' => true]) -> label(false) -> textInput(["placeholder" => "Nombre"])?>
 		
 				<?= $form->field($usuario, 'txt_apellido_paterno',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Apellido paterno", "class"=>"input-short animateds"])?>
 		
-				<?= $form->field($usuario, 'tel_numero_celular',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Número celular", "class"=>"animateds"])?>
+				<?= $form->field($usuario, 'tel_numero_celular',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Número celular"])?>
 			
-				<?= $form->field($usuario, 'num_esferas',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Número de esferas", "class"=>"animateds"])?>
+				<?= $form->field($usuario, 'num_esferas',['options'=>['class'=>'animated']]) -> label(false) -> textInput(["placeholder" => "Número de esferas"])?>
 			
 				<?= Html::submitButton('<span class="ladda-label">Registrar</span>', ['id'=>'js-submit-form', 'class' => 'btn btn-primary js-btn-registrar ladda-button animated', 'name' => 'contact-button', 'data-style'=>'zoom-out'])?>
 			
