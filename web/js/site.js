ba94d2e6a4100af7013832f9974f8a41a02305e4
cloud-elric <humberto@2gom.com.mx>
2016-10-13 19:17:42 -0500

    mensajes

--- web/js/site.js
+++ web/js/site.js
@@ -7,66 +7,83 @@ $('body').on(
 			if (form.find('.has-error').length) {
 				return false;
 			}
-			
+
 			var button = document.getElementById('js-submit-form');
 			var l = Ladda.create(button);
-		 	l.start();
-			
-			
+			l.start();
+
 			// submit form
 			$.ajax({
 				url : form.attr('action'),// url para peticion
 				type : 'post', // Metodo en el que se enviara la informacion
 				data : form.serialize(), // La informacion a mandar
-				dataType: 'json',  // Tipo de respuesta
-				success : function(response) { // Cuando la peticion sea exitosamente se ejecutara la funcion
+				dataType : 'json', // Tipo de respuesta
+				success : function(response) { // Cuando la peticion sea
+												// exitosamente se ejecutara la
+												// funcion
 					// Si la respuesta contiene la propiedad status y es success
 					if (response.hasOwnProperty('status')
 							&& response.status == 'success') {
-						
-						$(".aqua-body").fadeOut( 200 );
-						$(".aqua-body-succes").fadeIn( 300 );
+
+						$(".aqua-body").fadeOut(200);
+						$(".aqua-body-succes").fadeIn(300);
 						$(".aqua-body-succes").css("display", "flex");
 
-						var nombre = $("#entusuarios-txt_nombre").val()+" "+$("#entusuarios-txt_apellido_paterno").val();
+						var nombre = $("#entusuarios-txt_nombre").val() + " "
+								+ $("#entusuarios-txt_apellido_paterno").val();
 						var numEsferas = $("#entusuarios-num_esferas").val();
-						
-						var item = '<div class="aqua-registros-item">'+
-										'<p>'+nombre+'</p>'+
-										'<p>'+numEsferas+'</p>'+
-								'</div>';
-					
+
+						var item = '<div class="aqua-registros-item">' + '<p>'
+								+ nombre + '</p>' + '<p>' + numEsferas + '</p>'
+								+ '</div>';
+
 						document.getElementById("registro-form").reset();
-						
-						
+
 						$('#js-contenedor-registros').append(item);
 
-						$(".aqua-body-succes .animated").animate({ "opacity": "0" }, 300, function() {
-							$(".aqua-body-succes").show();
-							$(".aqua-body-succes .animated").each(function(index) {$( this ).addClass("delay-"+(index)+" fadeInUp");});
-						});
-						
+						$(".aqua-body-succes .animated").animate(
+								{
+									"opacity" : "0"
+								},
+								300,
+								function() {
+									$(".aqua-body-succes").show();
+									$(".aqua-body-succes .animated").each(
+											function(index) {
+												$(this).addClass(
+														"delay-" + (index)
+																+ " fadeInUp");
+											});
+								});
+
 					} else {
-						
+
 						// Muestra los errores
 						$('#registro-form').yiiActiveForm('updateMessages',
 								response, true);
 					}
-					
+
 					l.stop();
 				},
-				statusCode: {
-				    404: function() {
-				      alert( "page not found" );
-				    }
-				  }
+				statusCode : {
+					404 : function() {
+						alert("page not found");
+					}
+				}
 
 			});
 			return false;
 		});
 
+$('#registro-form').on(
+		'afterValidate',
+		function(e) {
 
-$('#registro-form').on('afterValidate', function (e) {
-	alert();
-    return false;
-});
+			if ($('#registro-form').find('.has-error').length) {
+				$("#message-error").text(
+						$('.has-error:eq(0) .help-block-error').text());
+				return false;
+			}
+
+		});
+
